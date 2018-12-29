853f6f1b9a6db5e17dc268ff12445753eca1c975
unknown <190572017@qq.com>
2018-12-29 20:17:40 +0800

    12.29

---
+++ app/src/main/java/com/example/administrator/zhailuprojecttest001/register/ChangePhoneActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.zhailuprojecttest001.register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.zhailuprojecttest001.R;
+
+public class ChangePhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_phone);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/zhailuprojecttest001/register/GetbackPw3Activity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.zhailuprojecttest001.register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.zhailuprojecttest001.R;
+
+public class GetbackPw3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_getback_pw3);
+    }
+}
